c7a5054559b2acc670b7bc58c19a7b882b931e85
Alex-Wenk <alexjwenk@gmail.com>
2023-09-17 16:50:12 +0100

    Cache AgloConfigurations in AlgoConfigurationService
    Given there will be of order 10k AlgoConfigurations in total it is cheap and safe to cache the configurations once they are loaded instead of retrieving them on each signal.

--- src/main/java/com/db/algotradesignal/signal/configuration/AlgoConfigurationService.java
+++ src/main/java/com/db/algotradesignal/signal/configuration/AlgoConfigurationService.java
@@ -2,17 +2,38 @@ package com.db.algotradesignal.signal.configuration;
 
 import com.db.algotradesignal.signal.configuration.persistence.AlgoConfigurationRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class AlgoConfigurationService {
+    private static final Logger log = LoggerFactory.getLogger(AlgoConfigurationService.class);
+
 
     private final AlgoConfigurationRepository algoConfigurationRepository;
+    private final HashMap<Integer, AlgoConfiguration> algoConfigBySignalId = new HashMap<>();
 
     public Optional<AlgoConfiguration> getAlgoConfigurationForSignal(int signalId) {
-        return algoConfigurationRepository.findBySignalId(signalId).map(AlgoConfiguration::fromEntity);
+        return Optional.ofNullable(algoConfigBySignalId.get(signalId))
+                .or(() -> findAndCacheConfig(signalId));
+    }
+
+    private Optional<AlgoConfiguration> findAndCacheConfig(int signalId) {
+        Optional<AlgoConfiguration> config = algoConfigurationRepository.findBySignalId(signalId)
+                .map(AlgoConfiguration::fromEntity);
+
+        config.ifPresent(this::cacheAlgoConfiguration);
+
+        return config;
+    }
+
+    private void cacheAlgoConfiguration(AlgoConfiguration configuration) {
+        log.info("Caching configuration for signal [%s]".formatted(configuration.getSignalId()));
+        algoConfigBySignalId.put(configuration.getSignalId(), configuration);
     }
 }
